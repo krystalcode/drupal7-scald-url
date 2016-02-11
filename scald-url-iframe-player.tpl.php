bdb78d1076091616c153d157a22a0972be903bf3
Dimitris Bozelos <dbozelos@gmail.com>
2016-02-11 17:35:32 +0000

    initial commit with first working version of the module

---
+++ scald-url-iframe-player.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for the Scald URL Iframe Player.
+ */
+?>
+
+<iframe width="<?php print $vars['iframe_width'] ?>" height="<?php print $vars['iframe_height'] ?>" src="<?php print $vars['iframe_url'] ?>" frameborder="0" allowfullscreen></iframe>
